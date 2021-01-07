efdad3848dea1d82e6488821557e53d5247ed53c
guykem123 <50095264+guykem123@users.noreply.github.com>
2021-01-07 10:52:40 +0200

    change auth message

--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -2,7 +2,7 @@ const jwtService = require("../services/jwtService");
 
 exports.ensureAuth = function (req, res, next) {
   if (!req.headers.authorization) {
-    return res.status(403).send({ message: "Auth failed" });
+    return res.status(403).send({ message: "Forbidden: Please Provide a Token..." });
   }
   try {
     const token = req.headers.authorization.split(" ")[1];
