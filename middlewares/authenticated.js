0ba4e0762b310d838324265820f485ec979a7d58
guykem123 <50095264+guykem123@users.noreply.github.com>
2021-01-11 18:19:13 +0200

    remove auth

--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -1,17 +1,17 @@
 const jwtService = require("../services/jwtService");
 
 exports.ensureAuth = function (req, res, next) {
-  if (!req.headers.authorization) {
-    return res.status(403).send({ message: "Forbidden: Please Provide a Token..." });
-  }
-  try {
-    const token = req.headers.authorization.split(" ")[1];
-    const decode = jwtService.verifyToken(token)
-    if (!decode || decode.exp > Date.now()) {
-      return res.status(401).send({ message: "Expired Token." });
-    }
-  } catch (ex) {
-    return res.status(403).send({ message: "Forbidden: Invalid Token..." });
-  }
+  // if (!req.headers.authorization) {
+  //   return res.status(403).send({ message: "Forbidden: Please Provide a Token..." });
+  // }
+  // try {
+  //   const token = req.headers.authorization.split(" ")[1];
+  //   const decode = jwtService.verifyToken(token)
+  //   if (!decode || decode.exp > Date.now()) {
+  //     return res.status(401).send({ message: "Expired Token." });
+  //   }
+  // } catch (ex) {
+  //   return res.status(403).send({ message: "Forbidden: Invalid Token..." });
+  // }
   next();
 };
